7465e5e69d45ac6cc9f3c87af1e7fecd8cf7cf98
Christopher Jung <jungc2.coventry.ac.uk>
2017-11-03 17:20:33 +0000

    Can now recieve post requests

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
+import simplejson
 
 hostName = "localhost"
 hostPort = 9000
@@ -10,8 +11,21 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>Gym-Chat-Bot</title></head>", "utf-8"))
-        self.wfile.write(bytes("<body><p>Wowzers</p>", "utf-8"))
+        self.wfile.write(bytes("<body><form method=\"post\"><input type=\"submit\"> </form>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
+
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+        self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+        self.wfile.write(bytes("{'Cake': '5'}", "utf-8"))
+
+        data = simplejson.loads(self.data_string)
+        print ("{}".format(data))
+        self.wfile.write(bytes("{'wow': 'okay'}", "utf-8"))
+        return
 
 myServer = HTTPServer((hostName, hostPort), MyServer)
 print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
