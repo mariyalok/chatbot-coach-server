60e334084045aa76bb336fa1ec935de018c4c5f0
Angus Simpson <simps110@uni.coventry.ac.uk>
2017-11-03 17:42:07 +0000

    Start to sentence parsing

---
+++ sentenceParsing.py
@@ -0,0 +1,9 @@
+musclePrint = "So Today you would like to work your " + userInput + "?"
+
+userInput = input("What would you like to do today?")
+
+muscleGroupsList = [Triceps, Biceps, Shoulders, Back, Quadraceps]
+
+Def muscleGroupsFunc:
+if userInput in muscleGroupsList:
+    
