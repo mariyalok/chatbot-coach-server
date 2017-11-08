63078841714681bece5ee5f8e49db14d7c5640cb
Angus Simpson <simps110@uni.coventry.ac.uk>
2017-11-08 18:59:40 +0000

    Removed unnecessary code

--- sentenceParsing.py
+++ sentenceParsing.py
@@ -1,37 +1,40 @@
+"""Sentence Parsing code"""
 
-
+#Defining of lists and dictionaries, strength & conditioning.
 
 highList = ['treadmill','bike','cross trainer','rowing machine']
 medList = ['treadmill','bike','cross trainer','rowing machine']
 lowList = ['treadmill','bike','cross trainer']
 intervalList = ['bike','cross trainer','rowing machine']
 
-cardioDictionary = {'high intensity': highList,'medium intensity':medList,'low intensity':lowList,'interval training':intervalList}
+cardioDictionary = {'high intensity':highList,'medium intensity':medList,'low intensity':lowList,'interval training':intervalList}
 
 
 
-definitionList = ['muscular definition','vascular definition']
-sizeList = []
-shapeList = []
 
-aestheticsList = [definition, size, shape]
+staticList = ['core','back','shoulders','arms','glutes','calves','quadraceps','chest']
+explosiveList = ['core','back','shoulders','arms','glutes','calves','quadraceps','chest']
+dynamicList = ['core','back','shoulders','arms','glutes','calves','quadraceps','chest']
+enduranceList = ['core','back','shoulders','arms','glutes','calves','quadraceps','chest']
 
-
-
-staticList = []
-explosiveList = []
-dynamicList = []
-
-strengthList = [static, explosive, dynamic]
+strengthDictionary = {'static':staticList,'explosive':explosiveList,'dynamic':dynamicList,'endurance':enduranceList}
 
 
 
-globalDictionary = {'cardio':cardioDictionary, 'aesthetics':aestheticsList, 'strength':strengthList}
+
+globalDictionary = {'cardio':cardioDictionary,'strength':strengthList}
 
 
-for target in cardioList:
-    print(target)
-    for x in target:
-        print(x)
-        if 'head' in x:
-            print("good")
+
+#~~~~~~~~~~# identification of input #~~~~~~~~~~#
+
+
+#Cardio identification
+
+if 'cardio' in Input:
+    print("What type of Cardiovascular training would you like to do, your options are:\n ")
+    print("High intensity training\n ")
+    print("Medium intensity training\n ")   
+    print("Low intensity training\n ")
+    print("interval training\n ")
+if ('cardio' and 'high intensity') or 
