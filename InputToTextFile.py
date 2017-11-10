bfc8db94c3143693c6e1b8c90bcaf282ba962b8f
Steelswarm <belezama@uni.coventry.ac.uk>
2017-11-10 22:45:33 +0000

    Fixed typos

---
+++ InputToTextFile.py
@@ -0,0 +1,49 @@
+
+import simplejson as json
+data_dict = {} #dict
+tempList=[] #Temporary lists where all the data will be saved in the txt file.
+
+def generatePrimaryResponse(month,day): # Mariya & Antonio
+    
+    if month in data_dict:# If the month is already in the dict it checks if the day is in it too
+        if day in data_dict[month]:
+            pass # If there is already a day in the dict it skips it.
+        else:
+            data_dict[month] = {str(day):0} #If not it adds it and adds hours as 0.
+            
+    else:
+        data_dict[month] = {str(day):0} # If not, it appends it as a key to the dict, the variable day as a key to the value of month and hours as a value of day where it is initialized as 0.
+        
+        
+    tempList.append(month) #Adds the month to the list to write it in the txt file.
+    tempList.append(str(day)) #Adds the day to the lsit to write it in the txt file.
+    hour= input("How many hours did you work on that day?")#Joao
+    return generateSecondaryResponse(hour)#Joao
+    depth += 1
+    
+
+def generateSecondaryResponse(hour):#Joao
+    data_dict[tempList[0]][tempList[1]] += hour # put the data(hours) in position data[0][1]
+    depth = 0
+    tempList.clear() #clean the tempList created
+    
+   
+    
+
+#Joao
+f=open('log.txt','w') # Opens the file,converts the list into a json string, writes on it and closes it
+f.write(json.dumps(data_dict))
+f.closed
+
+
+
+
+
+f=open('log.txt','r')# Opens the file, converts the string back into list, reads it, prints it and closes it.
+
+print(json.loads(f.read()))
+
+f.closed
+
+    
+    
