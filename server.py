3a52480b659403fa41dccff9bec5aa14321b286b
XrayNerd <jungc2@uni.coventry.ac.uk>
2017-11-17 17:06:30 +0000

    Added comments, reply is just now the output rather than getting each category

--- server.py
+++ server.py
@@ -3,27 +3,47 @@ import time
 import simplejson as json
 from sentenceParsing import *
 
+# Accept all connections on port 8000
+#
 hostName = "0.0.0.0"
 hostPort = 8000
 
 class MyServer(BaseHTTPRequestHandler):
+    # Method is called when GET request is made
     def do_GET(self):
+        # Deny GET method and return 401 unauthorised
         self.send_response(401)
 
+    # Method is called when POST request is made
     def do_POST(self):
+        # Tell client connection is accepted and
+        # Allow connections over different ports
+        #
         self.send_response(200)
         self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
+        # Read the input from client as a string
+        # 
         self.data_string = self.rfile.read(int(self.headers["Content-Length"]))
 
+        # Turns the JSON string into a dictionary
         data = json.loads(self.data_string)
+
         print ("Recieved: {} at {}".format(data, time.asctime()))
+        # Take keywords from message recieved and
+        # put them into a dict keyWords
         identifyOutput(data["message"])
-        data["message"] = getKeyWord("Strength") + getKeyWord("Cardio") 
+        data["message"] = returnOutput()
+
+        # Format dictionary into acceptable JSON
+        # format
         JSONstr = str(data).replace("'", '"').replace('\\"', "'").replace("\\'", "'").replace(': None', ': ""')
+        # Send the file back to the client in bytes
+        # in utf-8
         self.wfile.write(bytes(json.dumps(JSONstr), "utf-8"))
         return
 
+# initialise server
 myServer = HTTPServer((hostName, hostPort), MyServer)
 print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
 
