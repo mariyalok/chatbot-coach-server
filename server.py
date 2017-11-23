3d3c007d2ccd65d56df1f378e817dab11e3244e9
XrayNerd <jungc2@uni.coventry.ac.uk>
2017-11-23 16:16:08 +0000

    Fixed Merge

--- server.py
+++ server.py
@@ -26,20 +26,12 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
-<<<<<<< HEAD
-=======
         # Read the input from client as a string
         # 
->>>>>>> 83ac258d85b5c1117ab7435865ce4f201dcdedcd
         self.data_string = self.rfile.read(int(self.headers["Content-Length"]))
 
         # Turns the JSON string into a dictionary
         data = json.loads(self.data_string)
-<<<<<<< HEAD
-        JSONstr = str(data).replace("'", '"').replace('\\"', "'").replace("\\'", "'").replace(': None', ': ""')
-        print ("Recieved: {} at {}".format(data, time.asctime()))
-=======
-
         print ("Recieved: {} at {}".format(data, time.asctime()))
         # Take keywords from message recieved and
         # put them into a dict keyWords
@@ -53,7 +45,6 @@ class MyServer(BaseHTTPRequestHandler):
         JSONstr = str(data).replace("'", '"').replace('\\"', "'").replace("\\'", "'").replace(': None', ': ""')
         # Send the file back to the client in bytes
         # in utf-8
->>>>>>> 83ac258d85b5c1117ab7435865ce4f201dcdedcd
         self.wfile.write(bytes(json.dumps(JSONstr), "utf-8"))
         return
 
